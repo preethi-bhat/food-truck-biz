1016a9936c671378826be49ecfc4f79a3f8535b2
Anju Suryawanshi <asuryawanshi@SFOC02MQ17LFD58.sea.corp.expecn.com>
2015-02-12 13:10:40 -0800

    calling filter service

--- app.js
+++ app.js
@@ -28,27 +28,19 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    var status = err.status || 500;
+    var body = JSON.stringify({
+        message: err.message || 'an error occured',
+        status: status
     });
+    console.log(err.stack);
+    res.writeHead(status, {
+        'Content-Length': body.length,
+        'Content-Type': 'application/json'
+    });
+    res.write(body);
+    res.end();
 });
 
 
--- routes/car.js
+++ routes/car.js
@@ -1,44 +1,127 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var winston = require('winston');
 
-/*
- * GET userlist.
- */
-router.get('/results', function(req, res) {
+var SERVICE_PREFIX = 'Orchestration Service: '
+router.get('/results', function(req, res, next) {
+  request('http://localhost:3002/crs/results', function (error, response, body) {
+    if (error) {
+      winston.error(SERVICE_PREFIX + 'Received an error while calling CRS Service %s', error);
+      res.json('error');
+      next(error);
+      return;
+    } 
+    winston.info(SERVICE_PREFIX + 'Calling offer Service to process results : %s', data);
 
-var data ='';
-var request = require("request");
- 
-request('http://localhost:3002/crs/results', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        data = JSON.parse(body);
-        res.json(data);
-        
-		request({
-  			uri: "http://localhost:3100/log/warn",
-  			method: "POST",
-  			form: data
-  		});
-    } else {
-     	res.json('error');
-    }
-   process.on('uncaughtException', function (err) {
-    console.log(err);
+    request({
+        uri: "http://localhost:3003/offer/results",
+        method: "POST",
+        form: data,
+        },
+        function(error, response, body) {
+          console.log(body);
+          var offerRes = JSON.parse(body);
+          winston.info(SERVICE_PREFIX + 'Got processed  results from offer service: %s', offerRes);
+
+        res.json(offerRes)
+
+        });
+    
+  });
+
+  var sample = {
+    
+     "searchParam": {
+      "destination": "DAL",
+      "pickup": "HOU",
+      "dropOff": "DAL"
+   },
+   "searchId": "123",
+   "results": [
+        {
+          "id": "s1",
+          "pgoods": [
+            {
+              "pgoodId": "p1-1",
+              "carType": "ECO",
+              "carVendor": "Enterprize",
+              "price": "250",
+              "isOpaque": true,
+              "depositType": "Credit"
+
+            }
+          ]
+        },
+        {
+          "id": "s2",
+          "pgoods": [
+            {
+              "pgoodId": "p2-1",
+              "carType": "COM",
+              "carVendor": "Thrifty",
+              "price": "200",
+              "isOpaque": true,
+              "depositType": "Credit"
+            }
+          ]
+        },
+        {
+          "id": "s3",
+          "pgoods": [
+            {
+              "pgoodId": "p3-1",
+              "carType": "ECO",
+              "carVendor": "Dollar",
+              "price": "150",
+              "isOpaque": true,
+              "depositType": "Credit"
+            }
+          ]
+        },
+        {
+          "id": "s4",
+          "pgoods": [
+            {
+              "pgoodId": "p4-1",
+              "carType": "ECO",
+              "carVendor": "Enterprize",
+              "price": "230",
+              "isOpaque": false,
+              "depositType": "Debit"
+            }
+          ]
+        },
+        {
+          "id": "s5",
+          "pgoods": [
+            {
+              "pgoodId": "p5-1",
+              "carType": "ECO",
+              "carVendor": "MidWay",
+              "price": "170",
+              "isOpaque": false,
+              "depositType": "Debit"
+            }
+          ]
+        },
+      ]
+  }
+
+  request({
+    uri: 'http://localhost:3004/filter',
+    method: 'POST',
+    json: sample
+  }, function (error, response, filteredResponse) {
+      if (error) {
+        next(error);
+        return;
+      }
+      console.log("filtered response..")
+      console.log(filteredResponse); 
+      res.json(filteredResponse);
+     });
 });
-})
-
-});
-
-
-/*
- * POST to adduser.
- */
-router.post('/search', function(req, res) {
-    //res.msg('Stubbed response');
-});
-
-
 
 module.exports = router;
 
