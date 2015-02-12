103cdc416dac70675e0d785fa0311bb6b2fcf611
prbhat <prbhat@hotwire.com>
2015-02-12 13:13:54 -0800

    Updating orchestration service

--- routes/car.js
+++ routes/car.js
@@ -11,46 +11,34 @@ var SERVICE_PREFIX = 'Orchestration Service: '
  */
 router.get('/results', function(req, res) {
 
-  var data ='';
-  var request = require("request");
 
-  winston.info(SERVICE_PREFIX + 'Getting the car search response from CRS service');
-   
-  request('http://localhost:3002/crs/results', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          data = JSON.parse(body);
-          res.json(data);
-          winston.info(SERVICE_PREFIX + 'Got car results : %s', data);
-          
-      		request({
-        			uri: "http://localhost:3100/log/warn",
-        			method: "POST",
-        			form: data
-        		});
-      } 
-      else {
-        winston.error(SERVICE_PREFIX + 'Received an error while getting car results from CRS service. Error is %s', error);
-       	res.json('error');
-      }
-     process.on('uncaughtException', function (err) {
-      console.log(err);
-      winston.error(SERVICE_PREFIX + 'Received an error while getting car results from CRS service. Error is %s', err);
+var data ='';
+request('http://localhost:3002/crs/results', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        data = JSON.parse(body);
 
+	winston.info(SERVICE_PREFIX + 'Calling offer Service to process results : %s', data);
+
+		request({
+  			uri: "http://localhost:3003/offer/results",
+  			method: "POST",
+  			form: data,
+  			},
+  			function(error, response, body) {
+  				console.log(body);
+  				var offerRes = JSON.parse(body);
+  				winston.info(SERVICE_PREFIX + 'Got processed  results from offer service: %s', offerRes);
+
+				res.json(offerRes)
+
+  			});
+    		} else {
+    			   winston.error(SERVICE_PREFIX + 'Received an error while calling CRS Service %s', error);
+     			res.json('error');
+    		}
   });
-  })
 
-});
-
-
-/*
- * POST to adduser.
- */
-router.post('/search', function(req, res) {
-    //res.msg('Stubbed response');
-    winston.info(SERVICE_PREFIX + 'Got a car search request');
-});
-
-
+		})
 
 module.exports = router;
 
