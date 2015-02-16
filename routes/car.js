707f5d4dfa6acfce606cced53186e9f1f7af06ea
Preethi Bhat <prbhat@hotwire.com>
2015-02-16 15:54:58 -0800

    Merging winner selection in the orchestration service

--- routes/car.js
+++ routes/car.js
@@ -7,31 +7,33 @@ var SERVICE_PREFIX = 'Orchestration Service: '
 
 router.get('/results', function(req, res) {
 
-  var start = new Date().getTime();
+  
   var data ='';
   var offerRes ='';
   var priceRes ='';
 
   // ***************** CRS SERVICE *****************
   winston.info(SERVICE_PREFIX + 'Calling CRS service');
-  request('http://localhost:3002/crs/results', function (error, response, body) {
+  request('http://localhost:3002/crs/results', function (error, response, crsbody) {
     if (!error && response.statusCode == 200) {
-        crsRes = JSON.parse(body);
+        //crsRes = JSON.parse(body);
         winston.info(SERVICE_PREFIX + 'Received response from CRS service');
 
         // ***************** Cluster SERVICE *****************
+        var start = new Date().getTime();
         winston.info(SERVICE_PREFIX + 'Calling Clustering service');
         request({
             uri: "http://localhost:3003/cluster/results",
             method: "POST",
-            form: crsRes,
-        }, function(error, response, body) {
+            body: crsbody,
+        }, function(error, response, clusterbody) {
           if(error){
             winston.error('Received error from Clustering service: ', error);
             return;
           }
-          var offerRes = JSON.parse(body);
+          var offerRes = JSON.parse(clusterbody);
           winston.info(SERVICE_PREFIX + 'Received response from Clustering service');
+          var endCluster = new Date().getTime();
 
           // ***************** Pricing SERVICE *****************
           winston.info(SERVICE_PREFIX + 'Calling price service');
@@ -44,15 +46,34 @@ router.get('/results', function(req, res) {
               winston.error('Received error from Price Service');
               return;
             }
+
+          var startFilter = new Date().getTime();
           var priceRes = JSON.parse(pricedResponse);
           winston.info(SERVICE_PREFIX + 'Received response from price service');
+
+          // ***************** Winner selection SERVICE *****************
+          winston.info(SERVICE_PREFIX + 'Calling winner selection service');
+          request({
+              uri: "http://localhost:3006/car/winner",
+              method: "POST",
+              form: priceRes,
+          }, function(error, response, winnerResponse) {
+            if(error){
+              winston.error('Received error from winner selection Service');
+              return;
+            }
+
+          var startFilter = new Date().getTime();
+          var winnerSelectionResp = JSON.parse(winnerResponse);
+          winston.info(SERVICE_PREFIX + 'Received response from winner selection service');
+
 
           // ***************** Filter SERVICE *****************
           winston.info(SERVICE_PREFIX + 'Calling filter service');
           request({
               uri: 'http://localhost:3004/car/filter',
               method: 'POST',
-              form: priceRes,
+              form: winnerSelectionResp,
             }, function (error, response, filteredResponse) {
                 if (error) {
                   next(error);
@@ -82,9 +103,11 @@ router.get('/results', function(req, res) {
 
                 res.json(sortRS);
 
-                var executionTime = new Date().getTime() - start;
+                var endServiceCalls = new Date().getTime();
+                var totalExecutionTime = (endCluster - start) + (endServiceCalls - startFilter); 
 
-                winston.warn(SERVICE_PREFIX + 'Execution time(ms) = ', executionTime);
+                winston.warn(SERVICE_PREFIX + 'Execution time excluding CRS and Pricing (ms) = ', totalExecutionTime);
+               });
                });
                });
                });
